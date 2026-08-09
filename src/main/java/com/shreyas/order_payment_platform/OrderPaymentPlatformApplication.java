c6cd6f87d99ed4dc4b4d737bb24ab45ae6daaf5d
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-08-09 19:54:36 +0530

    Setup the project and their dependency

---
+++ src/main/java/com/shreyas/order_payment_platform/OrderPaymentPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.shreyas.order_payment_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPaymentPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderPaymentPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shreyas/order_payment_platform/OrderPaymentPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreyas.order_payment_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPaymentPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
