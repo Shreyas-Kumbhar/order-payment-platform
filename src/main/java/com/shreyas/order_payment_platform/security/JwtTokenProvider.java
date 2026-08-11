48882c79f27b7315c8f9634e9770bc497f8c753a
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-08-11 16:38:29 +0530

    Generate the JWT

--- src/main/java/com/shreyas/order_payment_platform/security/JwtTokenProvider.java
+++ src/main/java/com/shreyas/order_payment_platform/security/JwtTokenProvider.java
@@ -1,4 +1,43 @@
 package com.shreyas.order_payment_platform.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Date;
+
+@Component
 public class JwtTokenProvider {
+    @Value("${app.jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt.expiration-ms}")
+    private long expirationMs;
+
+    private SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
+
+    public String generateToken(Authentication authentication) {
+        String username= authentication.getName();
+        String role=authentication.getAuthorities().stream()
+                .findFirst()
+                .map(GrantedAuthority::getAuthority)
+                .orElse("USER");
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + expirationMs);
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("role", role)
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .signWith(getSecretKey())
+                .compact();
+    }
+
 }
