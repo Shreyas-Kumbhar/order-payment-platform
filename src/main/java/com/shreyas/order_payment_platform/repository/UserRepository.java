ba9e420d3594b4bb2ec76a71a7bfde35b5201225
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-08-11 10:42:10 +0530

    So far project is complied

--- src/main/java/com/shreyas/order_payment_platform/repository/UserRepository.java
+++ src/main/java/com/shreyas/order_payment_platform/repository/UserRepository.java
@@ -1,8 +1,7 @@
 package com.shreyas.order_payment_platform.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
-
+import com.shreyas.order_payment_platform.entity.User;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
