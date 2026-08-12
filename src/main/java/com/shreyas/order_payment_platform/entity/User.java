0ace562fe456534fd5ec6b9b502cdfff3c5186d7
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-08-12 18:56:02 +0530

    SecurityConfig

--- src/main/java/com/shreyas/order_payment_platform/entity/User.java
+++ src/main/java/com/shreyas/order_payment_platform/entity/User.java
@@ -2,10 +2,7 @@ package com.shreyas.order_payment_platform.entity;
 
 import com.shreyas.order_payment_platform.entity.enums.Role;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -14,6 +11,7 @@ import java.time.LocalDateTime;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Table(name="users")
 
 public class User {
--- src/main/java/com/shreyas/order_payment_platform/service/AuthService.java
+++ src/main/java/com/shreyas/order_payment_platform/service/AuthService.java
@@ -1,4 +1,74 @@
 package com.shreyas.order_payment_platform.service;
 
+import com.shreyas.order_payment_platform.dto.requests.LoginRequest;
+import com.shreyas.order_payment_platform.dto.requests.RegisterRequest;
+import com.shreyas.order_payment_platform.dto.responses.JwtResponse;
+import com.shreyas.order_payment_platform.entity.User;
+import com.shreyas.order_payment_platform.entity.enums.Role;
+import com.shreyas.order_payment_platform.repository.UserRepository;
+import com.shreyas.order_payment_platform.security.JwtTokenProvider;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public void register(RegisterRequest registerRequest) {
+
+
+        if (userRepository.findByUsername(registerRequest.getUsername()).isPresent()) {
+            throw new IllegalStateException("Username is already in use");
+        }
+
+
+        if (userRepository.findByEmail(registerRequest.getEmail()).isPresent()) {
+            throw new IllegalStateException("Email is already in use");
+        }
+
+
+        User user = User.builder()
+                .username(registerRequest.getUsername())
+                .email(registerRequest.getEmail())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .role(Role.USER)
+                .build();
+
+
+        userRepository.save(user);
+    }
+
+    public JwtResponse login(LoginRequest loginRequest) {
+
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+
+        String token = jwtTokenProvider.generateToken(authentication);
+
+        User user = userRepository.findByUsername(loginRequest.getUsername())
+                .orElseThrow(() ->
+                        new IllegalStateException("User not found after authentication"));
+
+        return new JwtResponse(
+                token,
+                user.getUsername(),
+                user.getRole().name()
+        );
+    }
 }
