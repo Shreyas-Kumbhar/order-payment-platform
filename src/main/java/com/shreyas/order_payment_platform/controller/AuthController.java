e5612b4c8c56107a9b1cf4547e7505412c7a03a9
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-08-12 19:52:00 +0530

    Testing Successfull

--- src/main/java/com/shreyas/order_payment_platform/controller/AuthController.java
+++ src/main/java/com/shreyas/order_payment_platform/controller/AuthController.java
@@ -1,4 +1,32 @@
 package com.shreyas.order_payment_platform.controller;
 
+import com.shreyas.order_payment_platform.dto.requests.LoginRequest;
+import com.shreyas.order_payment_platform.dto.requests.RegisterRequest;
+import com.shreyas.order_payment_platform.dto.responses.JwtResponse;
+import com.shreyas.order_payment_platform.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
 public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@Valid @RequestBody RegisterRequest registerRequest) {
+        authService.register(registerRequest);
+        return ResponseEntity.ok("User registered successfully");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
+        JwtResponse jwtResponse = authService.login(loginRequest);
+        return ResponseEntity.ok(jwtResponse);
+    }
 }
