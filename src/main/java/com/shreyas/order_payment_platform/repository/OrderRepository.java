94d085d3294bffcbf3385b5495f1c363b770bab6
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-08-14 18:06:25 +0530

    OrderRepository

--- src/main/java/com/shreyas/order_payment_platform/repository/OrderRepository.java
+++ src/main/java/com/shreyas/order_payment_platform/repository/OrderRepository.java
@@ -1,4 +1,11 @@
 package com.shreyas.order_payment_platform.repository;
 
-public class OrderRepository {
+import com.shreyas.order_payment_platform.entity.Order;
+import com.shreyas.order_payment_platform.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order,Integer> {
+    List<Order> findByUser(User user);
 }
