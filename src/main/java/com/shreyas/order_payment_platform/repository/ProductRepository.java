4799b813dcc00e61131531c3183bbb198646d5da
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-08-16 17:08:12 +0530

    Debugginf

--- src/main/java/com/shreyas/order_payment_platform/repository/ProductRepository.java
+++ src/main/java/com/shreyas/order_payment_platform/repository/ProductRepository.java
@@ -3,5 +3,5 @@ package com.shreyas.order_payment_platform.repository;
 import com.shreyas.order_payment_platform.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface  ProductRepository extends JpaRepository<Product,Integer> {
+public interface  ProductRepository extends JpaRepository<Product,Long> {
 }
