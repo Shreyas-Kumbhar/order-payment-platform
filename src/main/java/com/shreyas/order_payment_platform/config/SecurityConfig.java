fd07c405b15af049c4de888f65d23bd7345f16e9
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-08-16 17:17:53 +0530

    Update the SecurityConfig for the ProductController

--- src/main/java/com/shreyas/order_payment_platform/config/SecurityConfig.java
+++ src/main/java/com/shreyas/order_payment_platform/config/SecurityConfig.java
@@ -46,6 +46,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth->auth
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                                .requestMatchers("/api/products/**").permitAll()
+                                .requestMatchers("/api/products").hasRole("ADMIN")
                 .requestMatchers("/api/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 )
