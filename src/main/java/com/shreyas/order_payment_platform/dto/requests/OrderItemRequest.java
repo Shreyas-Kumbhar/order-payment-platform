d03bc3c39243950a5427f14075468312a5332859
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-08-17 19:43:59 +0530

    OrderItemRequest

---
+++ src/main/java/com/shreyas/order_payment_platform/dto/requests/OrderItemRequest.java
@@ -0,0 +1,15 @@
+package com.shreyas.order_payment_platform.dto.requests;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderItemRequest {
+    @NotNull(message = "Product ID is required")
+    private Long productId;
+    @Min(value=1, message = "Quantity must be at least 1")
+    private Integer quantity;
+}
