11c713f079650093955bb639eb153d2e74b3a6a1
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-08-17 19:47:30 +0530

    OrderRequest

--- src/main/java/com/shreyas/order_payment_platform/dto/requests/OrderItemRequest.java
+++ src/main/java/com/shreyas/order_payment_platform/dto/requests/OrderItemRequest.java
@@ -10,6 +10,7 @@ import lombok.Setter;
 public class OrderItemRequest {
     @NotNull(message = "Product ID is required")
     private Long productId;
+
     @Min(value=1, message = "Quantity must be at least 1")
     private Integer quantity;
 }
