7ab7a5d2c0e472c5541ae5c79336c703caa228ca
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-08-28 12:54:01 +0530

    Complete OrderController

--- src/main/java/com/shreyas/order_payment_platform/controller/OrderController.java
+++ src/main/java/com/shreyas/order_payment_platform/controller/OrderController.java
@@ -5,12 +5,12 @@ import com.shreyas.order_payment_platform.dto.responses.OrderResponse;
 import com.shreyas.order_payment_platform.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -21,6 +21,16 @@ public class OrderController {
     @PostMapping
     public ResponseEntity<OrderResponse> createOrder(@Valid @RequestBody OrderRequest orderRequest,
                                                      Authentication authentication) {
-        return ResponseEntity.ok(orderService.createOrder(orderRequest, authentication));
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderService.createOrder(orderRequest, authentication));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderResponse>> getAllOrders(Authentication authentication) {
+        return ResponseEntity.ok(orderService.getMyOrders(authentication));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderResponse> getOrderById(@PathVariable Long id, Authentication authentication) {
+        return ResponseEntity.ok(orderService.getOrderById(id, authentication));
     }
 }
