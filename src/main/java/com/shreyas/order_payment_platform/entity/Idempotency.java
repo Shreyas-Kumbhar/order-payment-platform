c7f7241229979622d74879b274c4681fbe0bfd51
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-08-29 18:53:57 +0530

    Idempotency Entity

---
+++ src/main/java/com/shreyas/order_payment_platform/entity/Idempotency.java
@@ -0,0 +1,28 @@
+package com.shreyas.order_payment_platform.entity;
+
+import com.shreyas.order_payment_platform.entity.enums.IdempotencyStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name="idempotency_keys")
+public class Idempotency {
+    @Id
+    private String key;
+    private String requestHash;
+
+    @Lob
+    private String responseBody;
+
+    @Enumerated(EnumType.STRING)
+    private IdempotencyStatus status;
+
+    private LocalDateTime createdAt;
+}
