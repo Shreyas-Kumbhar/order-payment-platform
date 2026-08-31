52a55131af677f01395a2188f22f83cb2c6e459a
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-08-31 18:26:57 +0530

    update --IdempotencyKey entity

--- src/main/java/com/shreyas/order_payment_platform/entity/IdempotencyKey.java
+++ src/main/java/com/shreyas/order_payment_platform/entity/IdempotencyKey.java
@@ -15,14 +15,28 @@ import java.time.LocalDateTime;
 @Table(name="idempotency_keys")
 public class IdempotencyKey {
     @Id
-    private String key;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="idempotency_key",nullable = false,unique = true)
+    private String idempotencyKey;
+
+    @Column(name="request_hash", nullable =false)
     private String requestHash;
 
     @Lob
+    @Column(name="response_body", nullable =false)
     private String responseBody;
 
     @Enumerated(EnumType.STRING)
     private IdempotencyStatus status;
 
+    @Column(updatable=false)
     private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist()
+    {
+        this.createdAt = LocalDateTime.now();
+    }
 }
