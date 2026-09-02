815411a640c7df6e617abdb3d48c2e571e36c826
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-09-02 23:12:49 +0530

    update --GlobalExceptionHandler

--- src/main/java/com/shreyas/order_payment_platform/service/OrderService.java
+++ src/main/java/com/shreyas/order_payment_platform/service/OrderService.java
@@ -34,7 +34,6 @@ public class OrderService {
 
     @Transactional
     public OrderResponse createOrder(OrderRequest request, Authentication authentication) {
-
         User user = userRepository.findByUsername(authentication.getName())
                 .orElseThrow(()-> new ResourceNotFoundException
                         ("User not found with username: " + authentication.getName()));
