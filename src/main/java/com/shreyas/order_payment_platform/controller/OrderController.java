4adf208b22b6dbb61b7ffdaf6fae31bd73348768
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-09-06 21:13:44 +0530

    update --OrderController-> GetOrderById

--- src/main/java/com/shreyas/order_payment_platform/controller/OrderController.java
+++ src/main/java/com/shreyas/order_payment_platform/controller/OrderController.java
@@ -31,7 +31,7 @@ public class OrderController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<OrderResponse> getOrderById(@PathVariable Long id) {
-        return ResponseEntity.ok(orderService.getOrderById(id));
+    public ResponseEntity<OrderResponse> getOrderById(@PathVariable Long id, Authentication authentication) {
+        return ResponseEntity.ok(orderService.getOrderById(id,  authentication));
     }
 }
