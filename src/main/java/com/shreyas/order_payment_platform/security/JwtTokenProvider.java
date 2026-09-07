483973b50eff5e5ffc87ae58b84dae9dcc61c1cf
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-09-07 14:16:50 +0530

    update JwtTokenProvider--> System.out.println() to Logger in slf4J

--- src/main/java/com/shreyas/order_payment_platform/security/JwtTokenProvider.java
+++ src/main/java/com/shreyas/order_payment_platform/security/JwtTokenProvider.java
@@ -11,7 +11,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
 import java.util.Date;
 
 @Component
