81f588ca5081afba5a17929cd8ed4037fca2653d
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-09-07 12:30:08 +0530

    updated --UserController

--- src/main/java/com/shreyas/order_payment_platform/controller/UserController.java
+++ src/main/java/com/shreyas/order_payment_platform/controller/UserController.java
@@ -3,6 +3,7 @@ package com.shreyas.order_payment_platform.controller;
 import com.shreyas.order_payment_platform.dto.responses.UserResponse;
 import com.shreyas.order_payment_platform.entity.User;
 import com.shreyas.order_payment_platform.repository.UserRepository;
+import com.shreyas.order_payment_platform.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -15,19 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
     private final UserRepository userRepository;
+    private final UserService userService;
 
     @GetMapping("/me")
     public ResponseEntity<UserResponse> getCurrentUser(Authentication authentication) {
-        String username = authentication.getName();
-
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new RuntimeException("User not found with username: " + username));
-        UserResponse response = UserResponse.builder()
-                .id(user.getId())
-                .username(user.getUsername())
-                .email(user.getEmail())
-                .build();
-
-        return ResponseEntity.ok(response);
+        UserResponse userResponse= userService.getCurrentUser(authentication.getName());
+        return ResponseEntity.ok(userResponse);
     }
 }
