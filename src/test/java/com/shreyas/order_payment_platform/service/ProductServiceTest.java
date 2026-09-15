9d3e486265f9a31444f0cf87a6745cf0100d4f9f
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-09-15 13:27:11 +0530

    Test class for ProductService : createProductTest is done

--- src/test/java/com/shreyas/order_payment_platform/service/ProductServiceTest.java
+++ src/test/java/com/shreyas/order_payment_platform/service/ProductServiceTest.java
@@ -1,10 +1,20 @@
 package com.shreyas.order_payment_platform.service;
 
+import com.shreyas.order_payment_platform.dto.requests.ProductRequests;
+import com.shreyas.order_payment_platform.dto.responses.ProductResponse;
+import com.shreyas.order_payment_platform.entity.Product;
 import com.shreyas.order_payment_platform.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
@@ -14,5 +24,32 @@ public class ProductServiceTest {
 
     @InjectMocks
     ProductService productService;
+
+    @Test
+    void createProduct_shouldReturnProduct(){
+        ProductRequests requests = new ProductRequests();
+        requests.setName("test");
+        requests.setDescription("test");
+        requests.setPrice(new BigDecimal("10.0"));
+        requests.setStockQuantity(100);
+
+        Product savedProduct = Product.builder()
+                .id(1L)
+                .name("test")
+                .description("test")
+                .price(new BigDecimal("10.0"))
+                .stockQuantity(100)
+                .build();
+
+        when(productRepository.save(any(Product.class))).thenReturn(savedProduct);
+
+        ProductResponse productResponse = productService.createProduct(requests);
+
+        assertThat("Product ID should match", productResponse.id().equals(1L));
+        assertThat("Product name should match", productResponse.name().equals(requests.getName()));
+        assertThat("Product description should match", productResponse.description().equals(requests.getDescription()));
+        assertThat("Product price should match", productResponse.price().equals(requests.getPrice()));
+        assertThat("Product stock quantity should match", productResponse.stockQuantity().equals(requests.getStockQuantity()));
+    }
 
 }
