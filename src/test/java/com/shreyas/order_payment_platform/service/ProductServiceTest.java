a08a6445d286736e73fb20ccf59dbd41121d2971
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-09-15 13:01:24 +0530

    Test class for ProductService 2 things

--- src/test/java/com/shreyas/order_payment_platform/service/ProductServiceTest.java
+++ src/test/java/com/shreyas/order_payment_platform/service/ProductServiceTest.java
@@ -1,4 +1,18 @@
 package com.shreyas.order_payment_platform.service;
 
+import com.shreyas.order_payment_platform.repository.ProductRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
+
+    @Mock
+    ProductRepository productRepository;
+
+    @InjectMocks
+    ProductService productService;
+
 }
