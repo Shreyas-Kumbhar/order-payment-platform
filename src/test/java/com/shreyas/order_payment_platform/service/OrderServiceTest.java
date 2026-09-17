c39f3c8533db59f094c7f2d27258d3525bfb25d5
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-09-17 12:50:32 +0530

    OrderServiceTest -- createOrder_shouldReturnInsufficientStockCorrectly() created

--- src/test/java/com/shreyas/order_payment_platform/service/OrderServiceTest.java
+++ src/test/java/com/shreyas/order_payment_platform/service/OrderServiceTest.java
@@ -23,6 +23,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -100,6 +101,42 @@ public class OrderServiceTest {
 
         assertThat(orderResponse.totalAmount()).isEqualTo(new java.math.BigDecimal("100.00"));
 
+    }
+
+    @Test
+    public void createOrder_shouldReturnInsufficientStockCorrectly(){
+        Product product=Product.builder()
+                .id(1L)
+                .name("Product 1")
+                .description("Product 1")
+                .price(new java.math.BigDecimal("10.00"))
+                .stockQuantity(1)
+                .build();
+
+        OrderItemRequest item1=new OrderItemRequest();
+        item1.setProductId(1L);
+        item1.setQuantity(2);
+
+        OrderRequest orderRequest=new OrderRequest();
+        orderRequest.setOrderItems(List.of(item1));
+
+        User user= User.builder()
+                .id(200L)
+                .username("testuser")
+                .email("testUser@gmail.com")
+                .password("testpassword")
+                .role(Role.USER)
+                .build();
+
+        Authentication authentication = mock(Authentication.class);
+        when(authentication.getName()).thenReturn("testuser");
+        when(idempotencyKeyRepository.findByIdempotencyKey("test-key")).thenReturn(Optional.empty());
+        when(userRepository.findByUsername("testuser")).thenReturn(Optional.of(user));
+        when(productRepository.findById(1L)).thenReturn(Optional.of(product));
+
+        assertThatThrownBy(() -> orderService.createOrder(orderRequest, "test-key", authentication))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Insufficient stock for product: Product 1");
     }
 
 }
