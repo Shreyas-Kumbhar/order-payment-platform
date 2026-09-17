f51f6677884f2bc78b2dd1a94821f93e049fe8ad
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-09-17 10:10:21 +0530

    OrderServiceTest -- createOrder_shouldReturnTotalCorrectly() creating

--- src/test/java/com/shreyas/order_payment_platform/service/OrderServiceTest.java
+++ src/test/java/com/shreyas/order_payment_platform/service/OrderServiceTest.java
@@ -1,13 +1,19 @@
 package com.shreyas.order_payment_platform.service;
 
+import com.shreyas.order_payment_platform.dto.requests.OrderItemRequest;
+import com.shreyas.order_payment_platform.dto.requests.OrderRequest;
+import com.shreyas.order_payment_platform.entity.Product;
 import com.shreyas.order_payment_platform.repository.IdempotencyKeyRepository;
 import com.shreyas.order_payment_platform.repository.OrderRepository;
 import com.shreyas.order_payment_platform.repository.ProductRepository;
 import com.shreyas.order_payment_platform.repository.UserRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 public class OrderServiceTest {
@@ -26,5 +32,38 @@ public class OrderServiceTest {
 
     @InjectMocks
     private OrderService orderService;
+
+    @Test
+    public void createOrder_shouldReturnTotalCorrectly() {
+
+        Product product1=Product.builder()
+                .id(1L)
+                .name("Product 1")
+                .description("Product 1")
+                .price(new java.math.BigDecimal("10.00"))
+                .stockQuantity(100)
+                .build();
+
+        Product product2=Product.builder()
+                .id(2L)
+                .name("Product 2")
+                .description("Product 2")
+                .price(new java.math.BigDecimal("20.00"))
+                .stockQuantity(100)
+                .build();
+
+        OrderItemRequest item1=new OrderItemRequest();
+        item1.setProductId(1L);
+        item1.setQuantity(2);
+
+        OrderItemRequest item2=new OrderItemRequest();
+        item1.setProductId(2L);
+        item1.setQuantity(4);
+
+        OrderRequest orderRequest=new OrderRequest();
+        orderRequest.setOrderItems(List.of(item1, item2));
+
+
+    }
 
 }
