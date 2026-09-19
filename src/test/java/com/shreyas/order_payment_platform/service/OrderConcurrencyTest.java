d8d889480e68da999bd1a442793d5717288a084c
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-09-19 19:10:24 +0530

    OrderConcurrencyTest created !

--- src/test/java/com/shreyas/order_payment_platform/service/OrderConcurrencyTest.java
+++ src/test/java/com/shreyas/order_payment_platform/service/OrderConcurrencyTest.java
@@ -1,4 +1,8 @@
 package com.shreyas.order_payment_platform.service;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class OrderConcurrencyTest {
 }
