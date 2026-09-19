3e58848a23cbde4ec02947b3c1f22c9e80992755
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-09-19 19:11:18 +0530

    OrderConcurrencyTest created !

--- src/test/java/com/shreyas/order_payment_platform/service/OrderConcurrencyTest.java
+++ src/test/java/com/shreyas/order_payment_platform/service/OrderConcurrencyTest.java
@@ -1,8 +1,28 @@
 package com.shreyas.order_payment_platform.service;
 
+import com.shreyas.order_payment_platform.repository.IdempotencyKeyRepository;
+import com.shreyas.order_payment_platform.repository.OrderRepository;
+import com.shreyas.order_payment_platform.repository.ProductRepository;
+import com.shreyas.order_payment_platform.repository.UserRepository;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class OrderConcurrencyTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @Mock
+    private OrderRepository orderRepository;
+
+    @Mock
+    private IdempotencyKeyRepository idempotencyKeyRepository;
+
+    @Mock
+    private OrderService orderService;
 }
