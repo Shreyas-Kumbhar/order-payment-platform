fed1a24a221bed10adc34a5ce6ab16e04cd7ff5b
Shreyas Kumbhar <kumbharshreyas07@gmail.com>
2026-09-20 19:59:59 +0530

    mocks created of --/ UserRepository and ProductRepository

--- src/test/java/com/shreyas/order_payment_platform/service/OrderConcurrencyTest.java
+++ src/test/java/com/shreyas/order_payment_platform/service/OrderConcurrencyTest.java
@@ -20,6 +20,7 @@ import java.util.Optional;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+
 @ExtendWith(MockitoExtension.class)
 public class OrderConcurrencyTest {
 
